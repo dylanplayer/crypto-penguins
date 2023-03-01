855d2aa5fdca0a8d484588b93168b621f7618040
Brooklyn Dippo <brooklyndippo@gmail.com>
2023-03-01 12:44:30 -1000

    move broken branch to new  branch

--- web/src/components/ShuffleButton.tsx
+++ web/src/components/ShuffleButton.tsx
@@ -2,6 +2,6 @@ export default function ShuffleButton() {
     return (
       <button
         className="max-w-[500px] uppercase text-violet-300 border-violet-600 border-2 bg-transparent hover:bg-violet-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:cursor-not-allowed disabled:opacity-50"
-      ><a href="/all_penguins">See all penguins </a></button>
+      ><a href="/all_penguins">See all penguins</a></button>
     )
 }
